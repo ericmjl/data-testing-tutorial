8d709eefd93a363b5fdce83e14051dd3e1ff3f65
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-11 11:38:05 -0500

    rebuilt pages

---
+++ docs/build.py
@@ -0,0 +1,18 @@
+import os
+
+files = [
+    'index',
+    'chapter1',
+    'chapter2',
+    'chapter3',
+    'chapter4',
+    'chapter5',
+    'chapter6',
+]
+
+for f in files:
+    os.system('pandoc {0}.md -s -H book.css -o {0}.html'.format(f))
+
+os.system('git add .')
+os.system('git commit -m "rebuilt pages"')
+os.system('git push')
