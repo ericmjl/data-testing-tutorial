2c0d69f99a13d84bfa4f125412d228535a0ebf2f
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-12 15:50:47 -0500

    updated pages

--- docs/build.py
+++ docs/build.py
@@ -1,5 +1,12 @@
 import os
 
+# Write an automatic index.
+index = '\
+# Data Testing Tutorial\n\
+\n\
+## Table of Contents\n\
+'
+
 files = [
     'index',
     'chapter1',
@@ -10,8 +17,16 @@ files = [
     'chapter6',
 ]
 
+with open('index.md', 'w+') as f:
+    f.write(index)
+    for fname in files:
+        if fname != 'index':
+            f.write('[{0}](./{0})\n'.format(fname))
+
+
 for f in files:
     os.system('pandoc {0}.md -s -H book.css -o {0}.html'.format(f))
+
 
 os.system('git add .')
 os.system('git commit -m "updated pages"')
---
+++ docs/index.md
@@ -0,0 +1,9 @@
+# Data Testing Tutorial
+
+## Table of Contents
+[chapter1](./chapter1)
+[chapter2](./chapter2)
+[chapter3](./chapter3)
+[chapter4](./chapter4)
+[chapter5](./chapter5)
+[chapter6](./chapter6)
