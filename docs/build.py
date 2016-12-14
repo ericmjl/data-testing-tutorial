b8198b385eca40999700e7eca68831f59fb36a24
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-13 20:44:35 -0500

    updated build script

--- docs/build.py
+++ docs/build.py
@@ -32,6 +32,6 @@ for f in files:
     os.system('pandoc {0}.md -s -H book.css -o {0}.html'.format(f))
 
 
-# os.system('git add .')
-# os.system('git commit -m "updated pages"')
-# os.system('git push')
+os.system('git add .')
+os.system('git commit')
+os.system('git push')
