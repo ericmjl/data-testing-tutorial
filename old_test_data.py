8b70cbdd148dd5158baeab32dcaa23f0d0786609
Eric Ma <ericmajinglong@gmail.com>
2017-05-12 11:35:06 -0400

    Replaced tabs with spaces

--- old_test_data.py
+++ old_test_data.py
@@ -1,15 +1,16 @@
-import pandas as pd 
+import pandas as pd
 
 data = pd.read_csv('data/Divvy_Stations_2013.csv', index_col=0)
 data_corrupt = pd.read_csv('data/Divvy_stations_2013_corrupt.csv', index_col=0)
 
+
 def test_column_latitude_dtype():
-	"""
-	Checks that the dtype of the 'Latitude' column is a float.
-	"""
-	def column_latitude_dtype(df):
+    """
+    Checks that the dtype of the 'Latitude' column is a float.
+    """
+    def column_latitude_dtype(df):
 
-		assert df['latitude'].dtype == float
+        assert df['latitude'].dtype == float
 
-	column_latitude_dtype(data)
-	column_latitude_dtype(data_corrupt)
+    column_latitude_dtype(data)
+    column_latitude_dtype(data_corrupt)
