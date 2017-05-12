5654dff2158d84e7cfab91b6d817a6972d89339e
Eric Ma <ericmajinglong@gmail.com>
2017-05-12 19:06:10 -0400

    updated datafuncs & tests

--- datafuncs.py
+++ datafuncs.py
@@ -2,15 +2,24 @@ import string
 
 
 def increment(x):
-    return x - 1
+    return x + 1
 
 
 def strip_punctuation(text):
-    exclude = string.punctuation
-    return ''.join(s for s in text if s not in exclude)
+    return ''.join(s for s in text if s not in string.punctuation)
 
 
 def bag_of_words(text):
     text = strip_punctuation(text)
     words = set(text.split(' '))
     return words
+
+
+def min_max_scaler(x):
+    """
+    Returns a numpy array with all of the original values scaled between 0 and
+    1.
+
+    Assumes the data are a numpy array.
+    """
+    return (x - x.min()) / (x.max() - x.min())
