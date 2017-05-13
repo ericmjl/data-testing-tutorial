c76e11af68acd9a291a2ef97e2f21c8560087c02
Eric Ma <ericmajinglong@gmail.com>
2017-05-13 19:24:17 -0400

    Added clip and eq_roots functions

--- datafuncs_soln.py
+++ datafuncs_soln.py
@@ -1,5 +1,6 @@
 import string
 import numpy as np
+import math
 
 
 def increment(x):
@@ -32,3 +33,23 @@ def min_max_scaler(x):
 
 def standard_scaler(x):
     return (x - x.mean()) / x.std()
+
+
+def clip(data, lower, upper):
+    data[data < lower] = lower
+    data[data > upper] = upper
+    return data
+
+
+def eq_roots(coefficients):
+    """
+    Returns the non-complex roots of a quadratic equation.
+    """
+    a, b, c = *coefficients
+    # there can be an assertion here that mirrors
+    # the assumption in the test function.
+    discriminant = b**2 - 4*a*c
+    assert discriminant >= 0
+    root1 = (-a + math.sqrt(b**2 - 4*a*c))/(2*a)
+    root2 = (-a - math.sqrt(b**2 - 4*a*c))/(2*a)
+    return root1, root2
